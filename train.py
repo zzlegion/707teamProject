6a21f2710b4e3b830b74aef7167f1bcf11a79314
longw010 <lwang010@gmail.com>
2017-12-03 17:32:36 -0500

    vgg train

---
+++ train.py
@@ -0,0 +1,54 @@
+
+# encoding: utf-8
+from keras.models import Model
+from keras.layers import Dense
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import CSVLogger
+import os
+from keras import applications
+from keras import optimizers
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten
+
+
+batch_size = 64
+
+epochs = 100
+TRAIN_DIR = "/home/long/Desktop/project/data/train"
+VALID_DIR = "/home/long/Desktop/project/data/val"
+
+if __name__ == "__main__":
+    train_datagen = ImageDataGenerator(width_shift_range=0.05, height_shift_range=0.05, vertical_flip=True)
+    train_generator = train_datagen.flow_from_directory(TRAIN_DIR, target_size=(200, 200), batch_size=batch_size, class_mode="binary")
+    image_numbers = train_generator.samples
+    #print(train_generator.class_indices)
+
+    test_datagen = ImageDataGenerator()
+    validation_generator = test_datagen.flow_from_directory(VALID_DIR, target_size=(200, 200), batch_size=batch_size, class_mode="binary")
+
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(3, 200,200))
+
+    for layer in base_model.layers[:5]:
+        layer.trainable = False
+
+    # Adding custom Layers
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation="relu")(x)
+    predictions = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    model.compile(optimizer=optimizers.SGD(lr=0.0001, momentum=0.9), loss="binary_crossentropy", metrics=['accuracy'])
+
+    csv_logger = CSVLogger('log.csv', append=True, separator=';')
+    early_stopping = EarlyStopping(patience=10)
+    check_pointer = ModelCheckpoint('vgg16_best.h5', verbose=1, save_best_only=True)
+    model.fit_generator(train_generator, steps_per_epoch=image_numbers // batch_size, epochs=epochs,
+                        validation_data=validation_generator, validation_steps=batch_size,
+	                    callbacks=[early_stopping, check_pointer])
